42ffcb2ea50ef50ae9b350043abfac9cb8ba6e38
asciiboi <56585404+asciiboi@users.noreply.github.com>
2022-09-01 19:00:36 +0200

    I did some shit.

--- prefabs/player/SpeechArea.gd
+++ prefabs/player/SpeechArea.gd
@@ -3,14 +3,16 @@ extends Area
 var overlap
 var can_speak = false
 
+func _ready():
+	Dialogic.load()
 
 func _process(delta):
 	can_speak = !!overlap
+	if Input.is_action_just_pressed("speak"):
+		if overlap and !get_parent().speaking:
+			var dialog = Dialogic.start(overlap[0].timeline_name)
+			add_child(dialog)
 
 func _physics_process(delta):
 	overlap = get_overlapping_areas()
-	if Input.is_action_just_pressed("speak"):
-		if overlap and !get_parent().speaking:
-			Dialogic.load()
-			var dialog = Dialogic.start(overlap[0].timeline_name)
-			add_child(dialog)
+	
